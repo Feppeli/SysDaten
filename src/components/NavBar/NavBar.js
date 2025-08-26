882b36686759c141ebf55edfdee2fcadaeb0ca1b
Douglas Daten <douglas.silva@daten.com.br>
2025-08-26 14:28:49 -0300

    add nav component

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,14 @@
+const NavBar = () => {
+    return (
+        <nav>
+            <h2>SysDaten</h2>
+            <ul>
+                <li>
+                    <a href='/' className='suportelink'>Home</a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar;
